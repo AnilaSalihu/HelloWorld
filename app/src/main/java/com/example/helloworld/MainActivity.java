7f993385af389d30c8904c3e0766498d78765561
Anila Salihu <anilasalihu5@gmail.com>
2020-11-05 11:02:08 +0100

    3

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnOK;
@@ -19,9 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("You have called onCreate method!");
         System.out.println("commited");
-        // public void onClick(View view) {​​​​​​​​
-        //    Toast.makeText(this, "This is a toast", Toast.LENGTH_LONG ).show();
-        //  }​​​​​​​​
+//         public void onClick(View view) {
+//            Toast.makeText(this, "This is button is clicked", Toast.LENGTH_LONG ).show();
+//          }
 
         btnOK = (Button) findViewById(R.id.btnOK);
         btnOK.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/helloworld/MainActivity3.java
+++ app/src/main/java/com/example/helloworld/MainActivity3.java
@@ -1,18 +1,15 @@
 package com.example.helloworld;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     Button btnOK2;
     TextView txt;
@@ -22,20 +19,17 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        txt = (TextView) findViewById(R.id.txtWelcome);
+        txt = (TextView) findViewById(R.id.txtGreeting);
         btnOK2 = (Button) findViewById(R.id.btnOK2);
         name = (EditText) findViewById(R.id.txtName);
 
         btnOK2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                txt.setText("Welcome " + name.getText().toString() + "!. If you want to calculate your change please click on button >> Change <<");
+                txt.setText("Welcome " + name.getText().toString() + "!");
                 txt.setTextColor(Color.GRAY);
 
             }
         });
-
-
     }
-
 }
